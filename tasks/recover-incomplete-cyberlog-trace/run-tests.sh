cd9e97995552615a1260f288228161195b72a9cd
Artas Yaskar <artasyaskar@gmail.com>
2025-07-31 14:51:25 +0500

    Update run-tests.sh

--- tasks/recover-incomplete-cyberlog-trace/run-tests.sh
+++ tasks/recover-incomplete-cyberlog-trace/run-tests.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-set -e
 
-# Navigate to the directory containing the script
-cd "$(dirname "$0")"
-
-echo ">> Running unit tests from: $(pwd)"
-
-# Run the test script
-python3 test_outputs.py
